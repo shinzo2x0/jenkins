4124ffd8291c9fb7a0914d9fdc222c51150c3a13
Jalen Minor <minorjalen@gmail.com>
2026-04-09 18:59:56 -0700

    updated tf file

--- 2-main.tf
+++ 2-main.tf
@@ -7,3 +7,28 @@ resource "aws_s3_bucket" "frontend" {
     Name = "Jenkins Bucket"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "frontend_public" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.frontend.arn}/*"
+      }
+    ]
+  })
+}
